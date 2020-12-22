ae7e3acf7f7722abf4aa204d024546ef29843617
Alok Singh <alokbeniwal@gmail.com>
2020-12-22 01:54:45 -0800

    add karate graph

--- src/GCN.jl
+++ src/GCN.jl
@@ -1,5 +1,22 @@
 module GCN
 
-# Write your package code here.
+using LightGraphs, MetaGraphs, SimpleWeightedGraphs
+
+A = [
+    [0., 1., 0., 0., 0.],
+    [1., 0., 1., 0., 0.],
+    [0., 1., 0., 1., 1.],
+    [0., 0., 1., 0., 0.],
+    [0., 0., 1., 0., 0.],
+]
+
+feats = reshape(1:5, :, 1)
+
+function karate_club_graph()
+    # Create the set of all members, and the members of each club.
+    include("karate_adjacency_matrix.jl")
+    G = Graph(adjacency_mat)
+
+end
 
 end
